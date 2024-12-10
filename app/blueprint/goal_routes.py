b0ba9a070019e52292e5e27b07be6b441a0d6c7d
Younes Aki <younes.akichouh@student.ehb.Be>
2024-12-10 15:14:28 +0100

    Update goal_routes.py

--- app/blueprint/goal_routes.py
+++ app/blueprint/goal_routes.py
@@ -1,6 +1,7 @@
 #Imports
 from flask import Blueprint, request, jsonify
 from ..models import SavingsGoal
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 #Make a Blueprint for goals
 goal_bp = Blueprint('goal_bp', __name__)
@@ -8,7 +9,23 @@ goal_bp = Blueprint('goal_bp', __name__)
 #Routes
 
 #Route for getting all goals
-@goal_bp.route('/', methods=['GET'])
+@goal_bp.route('/all', methods=['GET'])
 def get_goals():
     goals = SavingsGoal.query.all()
     return jsonify([goal.serialize() for goal in goals])
+
+#Route for getting current user goals
+@goal_bp.route('/user', methods=['GET'])
+@jwt_required()
+def getCurrentUser_goals():
+    try : 
+        user_id = get_jwt_identity()
+        
+        savingsGoal = SavingsGoal.query.filter_by(user_id=user_id).all()
+        if savingsGoal:
+            return jsonify([savingsGoal.serialize() for goal in savingsGoal]), 200
+        else:
+            return jsonify({"error": "You have no goals. Add one!"}), 404
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": str(e)}), 500
