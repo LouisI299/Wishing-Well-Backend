a19b5fb4fb1e476a17c953fe6afc18a3e46604a9
LouisI299 <louisingelbrecht@gmail.com>
2024-12-10 15:01:47 +0100

    Backend route to fetch a goal by id

--- app/blueprint/goal_routes.py
+++ app/blueprint/goal_routes.py
@@ -1,6 +1,7 @@
 #Imports
 from flask import Blueprint, request, jsonify
 from ..models import SavingsGoal
+from flask_jwt_extended import jwt_required
 
 #Make a Blueprint for goals
 goal_bp = Blueprint('goal_bp', __name__)
@@ -12,3 +13,17 @@ goal_bp = Blueprint('goal_bp', __name__)
 def get_goals():
     goals = SavingsGoal.query.all()
     return jsonify([goal.serialize() for goal in goals])
+
+#Route for getting a goal by ID
+@goal_bp.route('/<int:id>', methods=['GET'])
+@jwt_required()
+def get_goal(id):
+    try:
+        goal = SavingsGoal.query.get(id)
+        if goal:
+            return jsonify(goal.serialize())
+        else:
+            return jsonify({"error": "Goal not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
