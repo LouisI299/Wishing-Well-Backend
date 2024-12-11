75ed4afbd28bef68ea60ae5f2c71525cc99f4692
Younes Aki <younes.akichouh@student.ehb.Be>
2024-12-11 15:54:21 +0100

    Update settings_routes.py

--- app/blueprint/settings_routes.py
+++ app/blueprint/settings_routes.py
@@ -55,6 +55,44 @@ def edit_profile():
             print(f"Error updating profile: {e}")
             return jsonify({"msg": "Error updating profile", "error": str(e)}), 500
         
+#Route to change the user's password // 
+@settings_bp.route('/ChangePassword', methods=['POST'])
+@jwt_required()
+def change_password():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    try:
+        data = request.get_json()  # Get the JSON data from the request
+        old_password = data.get('old_password')
+        new_password = data.get('new_password')
+
+        if not old_password or not new_password:
+            return jsonify({"error": "Old and new passwords are required"}), 400
+
+        if new_password == old_password:
+            return jsonify({"error": "New password cannot be the same as the old password"}), 400
+
+        #Add additional password strength validation here
+
+        if check_password_hash(user.password, old_password):
+            user.password = generate_password_hash(new_password)
+            db.session.commit()
+            return jsonify({"msg": "Password changed successfully"}), 200
+        else:
+            return jsonify({"error": "Invalid old password"}), 401
+
+    except Exception as e:
+        db.session.rollback()
+        # Log the exception (replace with logging in production)
+        print(f"Error changing password: {e}")
+        return jsonify({"msg": "An error occurred while changing password"}), 500
+    
+
+
     
 
     
