96132f3185b1fbc0c40b97cae1eee9499be0b61a
nyomathys <nyo.mathys@student.ehb.be>
2025-01-06 15:49:43 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,22 @@
 from app import create_app
+from app import app
+from flask import current_app
 
 app = create_app()
 
 if __name__ == '__main__':
+    app.run()
+
+@app.route('/routes', methods=['GET'])
+def list_routes():
+    routes = []
+    for rule in current_app.url_map.iter_rules():
+        routes.append({
+            'route': rule.rule,
+            'methods': list(rule.methods),
+            'endpoint': rule.endpoint
+        })
+    return {'routes': routes}, 200
+
+if __name__ == "__main__":
     app.run()
