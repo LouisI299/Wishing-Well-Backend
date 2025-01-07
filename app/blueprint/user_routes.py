7df6128e7c904a3bc8901938dd88a85b90e9cb05
LouisI299 <louisingelbrecht@gmail.com>
2025-01-07 17:07:21 +0100

    Update user_routes.py

--- app/blueprint/user_routes.py
+++ app/blueprint/user_routes.py
@@ -134,7 +134,7 @@ def search_users():
         
         # Search for users that match the query in first_name or last_name (case-insensitive)
         users = User.query.filter(
-            (User.first_name.ilike(f'%{query}%')) | (User.last_name.ilike(f'%{query}%'))
+            (User.first_name.ilike(f'%{query}%')) | (User.last_name.ilike(f'%{query}%' & (User.id != user_id)))
         ).all()
 
         # Return serialized user data (except the logged-in user)
