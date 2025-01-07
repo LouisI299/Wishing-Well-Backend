6cea4c1c28ed08ebcf591eff4e369fb33d181597
nyomathys <nyo.mathys@student.ehb.be>
2025-01-07 23:03:14 +0100

    Update fix_badge_paths.py

--- fix_badge_paths.py
+++ fix_badge_paths.py
@@ -6,9 +6,15 @@ app = create_app()
 with app.app_context():
     badges = Badge.query.all()
     for badge in badges:
-        if badge.image_url.startswith("/static/images/badges//"):
+        if "//static/images/badges/" in badge.image_url:
+            badge.image_url = badge.image_url.replace("//static/images/badges/", "/static/images/badges/")
+            db.session.add(badge)
+        elif badge.image_url.startswith("/static/images/badges//"):
             badge.image_url = badge.image_url.replace("/static/images/badges//", "/static/images/badges/")
             db.session.add(badge)
-    
+        elif not badge.image_url.startswith("/static/images/badges/"):
+            badge.image_url = f"/static/images/badges/{badge.image_url}"
+            db.session.add(badge)
+
     db.session.commit()
     print("Badge paden succesvol gecorrigeerd!")
