285b1e0ed3c785a6b2542fbe14a80d28e7584a64
LouisI299 <louisingelbrecht@gmail.com>
2025-01-07 11:47:43 +0100

    Update friend_routes.py

--- app/blueprint/friend_routes.py
+++ app/blueprint/friend_routes.py
@@ -25,7 +25,7 @@ def get_friends():
         for friendship in friendships:
             if friendship.user_id1 == user_id:
                 friend_ids.append(friendship.user_id2)
-            else:
+            elif friendship.user_id2 == user_id:
                 friend_ids.append(friendship.user_id1)
         
         friends = User.query.filter(User.id.in_(friend_ids)).all()
